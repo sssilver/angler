bed418fc6535abaedad7ac0fa331b6e8ac8ef8ab
sssilver <sssilver@gmail.com>
2013-11-20 03:19:02 +0400

    Add Student

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,12 +2,28 @@ app.directive('scDatepicker', function() {
     return {
         restrict: 'A',
         link: function(scope, element, attrs) {
+            var startView = attrs.startView;
+
+            if (!startView)
+                startView = 2;
+
+            $(element).datetimepicker({
+                'startView': startView
+            });
+        }
+    };
+});
+
+app.directive('scDatetimepicker', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
             var mode = attrs.scDatepicker;
 
             if (!mode)
                 mode = 'days';
 
-            $(element).datepicker({
+            $(element).datetimepicker({
                 'viewMode': mode
             });
         }
